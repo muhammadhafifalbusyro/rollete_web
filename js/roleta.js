c3248823028ec6b2aa09cdef35b44d87affef966
Muhammad Hafif Al Busyro <muhammadhafifalbusyro@gmail.com>
2024-11-09 10:19:13 +0700

    project rollete

---
+++ js/roleta.js
@@ -0,0 +1,77 @@
+const items = [
+    ['Item 01', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 02', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 03', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 04', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 05', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 06', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 07', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 08', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 09', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 10', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 11', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 12', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 13', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 14', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 15', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 16', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 17', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 18', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 19', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 20', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 21', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 22', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 23', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+    ['Item 24', 1,'https://m.media-amazon.com/images/M/MV5BZTNjOWI0ZTAtOGY1OS00ZGU0LWEyOWYtMjhkYjdlYmVjMDk2XkEyXkFqcGc@._V1_.jpg'],
+]
+
+$('.div-roulette').on('click', function() {rodaARoda()});
+
+$(document).keypress(function(event) {
+    if(event.charCode == 13 || event.charCode == 32){
+        rodaARoda();
+    }
+});
+
+// Get modal element
+const modal = document.getElementById("myModal");
+// Get close button
+const closeBtn = document.querySelector(".close");
+
+
+// Close modal when the user clicks the 'x' button
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+  };
+  
+  // Close modal when user clicks outside the modal content
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+function rodaARoda(){
+    if(!$('#roulette').hasClass('girando')){
+        let weight = new Array();
+        items.forEach((item, key) => {
+            for (let i = 0; i < item[1]; i++) {
+                weight.push(key + 1);
+            }
+        });
+        let choosed = weight[Math.floor(Math.random() * weight.length)];
+        $('#roulette').removeAttr('class');
+        setTimeout(() => {
+            document.getElementById("roleta-audio").play();
+            return $('#roulette').addClass(`number-${choosed}`).addClass('girando');
+            
+        }, 50);
+
+        setTimeout(() => {
+            modal.style.display = "flex";
+            document.getElementById("modalImage").src = items[choosed-1][2]
+            $('#roulette').removeClass('girando');
+        }, 10000);
+    }
+}
+
